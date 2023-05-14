1ceb80879198f74fb386935b66b31cf58e38f4a7
Pusyuu <121094209+PusyuuOfficial@users.noreply.github.com>
2023-05-14 14:14:53 +0900

    Add files via upload

--- SystemFolder/PageData/s-yakudatenaipage2/main.php
+++ SystemFolder/PageData/s-yakudatenaipage2/main.php
@@ -1,6 +1,6 @@
   <main>
     <div class="show">
-      <video loop="true" width="1280" height="720" poster="../ImageData/NanimoyakunitatanaiPage.png" onclick="this.paused ? this.play() : this.pause();">
+      <video loop="true" width="1280" height="720" poster="../../ImageData/NanimoyakunitatanaiPage.png" onclick="this.paused ? this.play() : this.pause();">
         <source type="video/mp4" src="../../MusicData/hage.mp3">
       </video> 
     </div>
