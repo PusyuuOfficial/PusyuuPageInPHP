e7fa7234c9b78ba28426c489245b3da49287922a
Pusyuu <121094209+PusyuuOfficial@users.noreply.github.com>
2023-07-05 16:28:45 +0900

    Add files via upload

---
+++ SystemFolder/PageData/b-tukaimawasi/content.php
@@ -0,0 +1,7 @@
+<?php
+$description = 'なんかふと、イノベーションに対して使いまわしをするなと言われた場合どうなってしまうんだろう、という疑問からこの記事を書きました。';
+$title = 'PusyuuLog - 使いまわしとイノベーション';
+require_once('../HeaderAndFooter/header.php');
+?>
+<?php require_once('main.php'); ?>
+<?php require_once('../HeaderAndFooter/footer.php'); ?>
